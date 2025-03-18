ee02038abb80122d0693cfa26557f34ecb155f94
Zahran03 <zalfarizi502@gmail.com>
2025-03-19 05:28:37 +0700

    feat : create dashboard user posts

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,11 +1,50 @@
-import React from "react";
+import { getCollection } from "@/lib/db";
+import getAuthUser from "@/lib/getAuthUser";
+import { ObjectId } from "mongodb";
+import Link from "next/link";
 
-const page = () => {
+const Dashboard = async () => {
+  const user = await getAuthUser();
+
+  const postsCollection = await getCollection("posts");
+  const userPosts = await postsCollection
+    ?.find({
+      userId: ObjectId.createFromHexString(user.userId),
+    })
+    .sort({ $natural: -1 })
+    .toArray();
+
+  if (!userPosts) return <p>Failed to fetch data!</p>;
+  if (userPosts.length === 0) return <p>You don't have any posts</p>;
   return (
     <div>
-      <p>Dashboard</p>
+      <h1 className="title">Dashboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th className="w-3/6">Title</th>
+            <th className="w-1/6 sr-only">View</th>
+            <th className="w-1/6 sr-only">Edit</th>
+            <th className="w-1/6 sr-only">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userPosts.map((post) => (
+            <tr key={post._id.toString()}>
+              <td className="w-3/6">{post.title}</td>
+              <td className="w-1/6 text-blue-500">
+                <Link href={`/posts/show/${post._id.toString()}`}>View</Link>
+              </td>
+              <td className="w-1/6 text-green-500">
+                <Link href={`/posts/edit/${post._id.toString()}`}>Edit</Link>
+              </td>
+              <td className="w-1/6 text-red-500">delete</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-export default page;
+export default Dashboard;
